75f1eddad4deaa261da0a7510a9c8c4883efeefa
fmnb0516 <fmnb0516@mnb.bz>
2019-07-26 15:00:06 +0900

    update.

--- modules/plugins/web-admin-console/web.js
+++ modules/plugins/web-admin-console/web.js
@@ -27,8 +27,18 @@ const resolve = (map, key) => {
 	}
 };
 
+const dateformat = (d) => {
+	return d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + ""
+	+ " "
+	+ ( '0' + d.getHours() ).slice(-2) + ":"
+	+ ( '0' + d.getMinutes() ).slice(-2) + ":"
+	+ "00";
+};
+
 module.exports = (installer, context) => {
 	installer.resource("/web-admin-console", "admin");
+
+	const storageDir = context.baseDir + "/storage";
 
 	const md5 = (str) => {
 		const md5 = context.external("crypto").createHash('md5')
@@ -63,8 +73,47 @@ module.exports = (installer, context) => {
 				return initAssetDir(assetDir)
 					.then(() => [id, postFilePath, assetDir, data]);
 			}).then(data => {
-				successHandler("ok");
-			});
+				const textLine = [];
+				const futures = [];
+
+				const id = data[0];
+				const postFilePath = data[1];
+				const assetDir = data[2];
+
+				const metadata = data[3];
+				const images = resolve(metadata.page_values, "image");
+				const title = resolve(metadata.page_values, "title")[0];
+				const category = resolve(metadata.page_values, "category");
+				const description = resolve(metadata.page_values, "description")[0];
+
+				textLine.push("---");
+				textLine.push("title: \"" + title + "\"");
+				textLine.push("date: \"" + dateformat(new Date()) + "\"");
+				textLine.push("thumbnail: \"" + "/images/"+id+"/"+ images[0] + "\"");
+				textLine.push("tags:");
+				category.forEach(element => {
+					textLine.push("- \"" + element + "\"");
+				});
+				textLine.push("---");
+		
+				textLine.push("");
+
+				for(var i=0; i < images.length; i++) {
+					const f = images[i];
+					textLine.push("!["+f+"](/images/"+id+"/"+f+")");
+					
+					if(i==0) {
+						textLine.push("<!-- more -->");
+						textLine.push("");
+						textLine.push(description);
+						textLine.push("");
+					}
+					futures.push(context.fileSystem.copy(storageDir + "/" + images[i], assetDir + "/" + images[i]));
+				}
+
+				futures.push(context.fileSystem.writeFile(postFilePath,textLine.join("\r\n"), "utf8"));
+				return Promise.all(futures);
+			}).then(() => successHandler("ok"));
 		
 	});
 
