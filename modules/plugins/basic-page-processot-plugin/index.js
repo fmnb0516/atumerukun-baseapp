535c133342a4a17f832f21e9ecc14bc572a336e5
fmnb0516 <fmnb0516@mnb.bz>
2019-07-30 10:47:50 +0900

    update.

--- modules/plugins/basic-page-processot-plugin/index.js
+++ modules/plugins/basic-page-processot-plugin/index.js
@@ -1,7 +1,6 @@
 module.exports = (appContext) => {
-
     appContext.pageProcessInstaller.install("blog-type-entries", require("./blog-type-entries.js")(appContext));
     appContext.pageProcessInstaller.install("page-data-resolve", require("./page-data-resolve.js")(appContext));
     appContext.pageProcessInstaller.install("page-data-persistence", require("./page-data-persistence.js")(appContext));
-
+    appContext.webApiInstaller.resource("/form", "form");
 };
