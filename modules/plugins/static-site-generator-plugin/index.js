8d60ec7e12ba8d5c2ce598cc3712b5fad9272a6e
fmnb0516 <fmnb0516@mnb.bz>
2019-07-30 10:55:20 +0900

    update.

--- modules/plugins/static-site-generator-plugin/index.js
+++ modules/plugins/static-site-generator-plugin/index.js
@@ -1,4 +1,7 @@
-module.exports = (appContext) => {
+module.exports = async (appContext) => {
+    const moduleDir = appContext.dir;
+    const storageDir = appContext.core.baseDir + "/storage";
 
-    
+    const configure = await appContext.core.fileSystem.readFile(moduleDir + "/configure.json", 'utf8')
+        .then(text => JSON.parse(text))    
 };
