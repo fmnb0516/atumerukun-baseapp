340f7c2a9547f1ed1268e6d523856dcacb16b9b5
fmnb0516 <fmnb0516@mnb.bz>
2019-08-05 16:31:47 +0900

    update.

--- modules/plugins/static-site-generator-plugin/lib/template.js
+++ modules/plugins/static-site-generator-plugin/lib/template.js
@@ -17,6 +17,10 @@ module.exports = async (appContext, themeDir, utils) => {
         tpl[templateFiles[i]] = Handlebars.compile(text);
     }
     
+    Handlebars.registerHelper('template', function(options) {
+        return options.fn(this);
+    });
+
     Handlebars.registerHelper('split', function (text, max, options) {
         const ary = text.split(",");
 
@@ -36,7 +40,6 @@ module.exports = async (appContext, themeDir, utils) => {
         return new Handlebars.SafeString(result.join(''));
     });
 
-    
     Handlebars.registerHelper('utc', function (val, options) {
         if(val === undefined || val === null) {
             return "";
