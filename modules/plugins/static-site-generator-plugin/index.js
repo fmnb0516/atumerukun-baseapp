4ecb2f3f0c4f430351ce2dccd0ce9657e0c5b773
fmnb0516 <fmnb0516@mnb.bz>
2019-08-06 13:37:58 +0900

    update.

--- modules/plugins/static-site-generator-plugin/index.js
+++ modules/plugins/static-site-generator-plugin/index.js
@@ -127,6 +127,21 @@ module.exports = async (appContext) => {
         await postgen(id, postFilePath, assetDir, data);
     };
 
+    appContext.webApiInstaller.put('/sitegenerator/configure', (req, res) => {
+        const successHandler = createSuccessHandler(req, res);
+        const json = JSON.stringify(req.body);
+        appContext.core.fileSystem.writeFile(moduleDir + "/configure.json", json, 'utf8')
+            .then(() => successHandler("ok"));
+     });
+
+    appContext.webApiInstaller.get('/sitegenerator/configure', (req, res) => {
+        appContext.core.fileSystem.readFile(moduleDir + "/configure.json", 'utf8')
+            .then(text => JSON.parse(text))
+            .then(json => {
+                res.json(json);
+            });
+    });
+
     appContext.webApiInstaller.post('/sitegenerator/all', (req, res) => {  
         const successHandler = createSuccessHandler(req, res);
         generateAllPosts().then(() => successHandler("site generated"));
@@ -148,6 +163,79 @@ module.exports = async (appContext) => {
             confirm_message : "解析結果を公開しますか",
             method: "POST"
         });
+    });
+
+    appContext.webApiInstaller.get('/console', (req, res) => {
+		
+		res.send(`
+				<!DOCTYPE html>
+				<html style="height: 100%;">
+					<head>
+						<meta charset="utf-8">
+						<title>Torasan: [webscraping-data-crud]</title>
+						<meta name="viewport" content="width=device-width, initial-scale=1">
+						<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+						<link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.css" media="screen">
+					</head>
+					<body style="height: 100%;">
+						
+						<div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+							<div class="container">
+								<a href="#" class="navbar-brand">Atumerukun - [static-site-generator-plugin] - console</a>
+							</div>
+						</div>
+						
+						<div class="container" style="padding-top: 80px;height: 100%;">
+							<div class="row" style="height: 100%;">
+								<div class="col-lg-12">
+									<div class="form-group" style="height: calc(100% - 80px);">
+										<label for="responseOutput">configure (json)</label>
+										<textarea class="form-control" id="responseOutput" style="height: calc(100% - 20px);"></textarea>
+                                        <br/>
+                                        <button type="button" class="btn btn-primary" id="save-btn">SAVE</button>
+									    <button type="button" class="btn btn-info" id="load-btn">LOAD</button>
+                                    </div>
+								</div>
+							</div>
+						</div>
+						
+						<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+						<script type="text/javascript">
+							$("#load-btn").click(function() {
+								$.ajax({
+									url:"./sitegenerator/configure",
+									type:"get",
+									dataType: 'json',
+									success: function(data) {
+										$("#responseOutput").val(JSON.stringify(data, null , "\t"));
+									},
+									error: function(XMLHttpRequest, textStatus, errorThrown) {
+										alert(errorThrown);
+									},
+								});
+							});
+							
+							$("#save-btn").click(function() {
+                                var parameter = $("#responseOutput").val().trim();
+                                
+								$.ajax({
+									url:"./sitegenerator/configure",
+									type:"put",
+									dataType: 'json',
+									contentType: 'application/json',
+									data: parameter,
+									success: function(data) {
+                                        alert(data.result);
+									},
+									error: function(XMLHttpRequest, textStatus, errorThrown) {
+										alert(errorThrown);
+									},
+								});
+							});
+						</script>
+					</body>
+				</html>
+		`.trim());
     });
 
     const schedule = configure.generate.schedule
